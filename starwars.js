e3a4cdb08ebbc25f2ecc5d0c09dd524aa07e899c
Jorge Dantas <jatad.tech@gmail.com>
2026-05-06 09:36:06 -0300

    Exercicio1-concluido

--- starwars.js
+++ starwars.js
@@ -4,4 +4,18 @@
 //  - Pegar a lista de filmes (AJAX) e preencher no HTML
 //  - Quando um filme for clicado, exibir sua introdução
 
+//Exercício 1: player de música
+// (a) importe essa função play do music.js no seu arquivo starwars.js
+// (b) invoque-a passando 2 argumentos referentes aos parâmetros especificados.
+import {play} from './music.js'
+let musica = {
+  audioUrl:         'audio/tema-sw.mp3',
+  coverImageUrl:    'imgs/logo.svg',
+  title:            'Intro',
+  artist:           'John Williams'
+}
+
+play(musica, document.body)
+
+
 const API_ENDPOINT = 'https://swapi.info/api'
